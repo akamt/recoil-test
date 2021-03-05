76e8a7e21dbdd93c6b889c9c0d8ccd4cc354755f
akamt <akamt68@gmail.com>
2021-03-06 07:06:53 +0900

    recoilお試し

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import {
+  atom,
+  RecoilRoot,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+
+const textState = atom({ key: "textState", default: "" });
+
+const charCounter = selector({
+  key: "charCountState",
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+const TextInput = () => {
+  const [text, setText] = useRecoilState(textState);
+
+  const count = useRecoilValue(charCounter);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <p>input: {text}</p>
+      <p>count: {count}</p>
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <RecoilRoot>
+    <TextInput />
+  </RecoilRoot>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
